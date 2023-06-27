d0f44582327acdeefaaa784bffefe93e63cfad46
lilbutterflly <tsara6317@gmail.com>
2023-06-27 09:21:45 -0700

    shell

--- shell.h
+++ shell.h
@@ -94,8 +94,8 @@ typedef struct passinfo
 	int histcount;
 } info_t;
 
-#define INFO_INT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \ 0, 0, 0}
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0, 0}
 /**
   * struct builtin - contains a builting string and related function
   * @type: the builtin command flag
